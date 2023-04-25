c701085732d4af129cf60e6789e394e75b04a51e
praveen4599 <praveen.nagarajan.45@gmail.com>
2023-04-25 19:18:51 +0530

    jest

--- Tasks/Task7/function.test.js
+++ Tasks/Task7/function.test.js
@@ -10,12 +10,40 @@ test('Add 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2,2)).not.toBe(5);
 });
 
+// toBeNull
 test('Should be null', () => {
 
     expect(functions.isNull()).toBeNull();
 });
+ 
+// Basically to be is for the primitive types 
 
-test('Should be falsy', () => {
+// toEqual
+test('User should be Brad Traversy object', () => {
 
-    expect(functions.checkValue(2)).toBeFalsy();
+    expect(functions.createUser()).toEqual({ 
+        firstName: 'Brad', 
+        lastName: 'Traversy' 
+    });
+});
+
+// Less than and Greater than
+test('Should be under 1600', () => {
+
+    const load1 = 800;
+    const load2 = 700;
+    expect((load1 + load2) < 1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+
+    expect('teami').not.toMatch(/I/);
+});
+
+// Arrays
+test('Admin should be in usernames', () => {
+
+    usernames = ['john', 'karen', 'admin'];
+    expect(usernames).toContain('admin');
 });
